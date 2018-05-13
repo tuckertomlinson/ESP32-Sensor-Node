750dfa43311699e04b453c3ce09b06c3b54f5540
Tucker <tuckertomlinson@gmail.com>
2018-05-12 19:34:51 -0500

    added serial print code for debugging SNTP

--- main/ESP32-Sensor-Node.c
+++ main/ESP32-Sensor-Node.c
@@ -173,5 +173,20 @@ void app_main()
         // update 'now' variable with current time
         time(&now);
     }
+    char strftime_buf[64];
+
+    // Set timezone to Eastern Standard Time and print local time
+    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    tzset();
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
+
+    // Set timezone to China Standard Time
+    setenv("TZ", "CST-8", 1);
+    tzset();
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
 }
 
